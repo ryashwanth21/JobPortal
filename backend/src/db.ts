52d03bb0d39d4423f630ab93ade452631c8e2919
ryashwanth21 <154409820+ryashwanth21@users.noreply.github.com>
2025-04-21 17:37:51 +0530

    Update db.ts

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -4,7 +4,7 @@ import mysql from 'mysql2/promise';
 export const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '',# Have ur password
+  password: '',//Have ur password
   database: 'job_portal',
   waitForConnections: true,
   connectionLimit: 10,
